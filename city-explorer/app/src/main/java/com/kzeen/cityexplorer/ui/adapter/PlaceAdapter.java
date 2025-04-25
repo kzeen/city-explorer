74216964ce7db3ff5305452a0760b35757177084
zeeny <157370421+kzeen@users.noreply.github.com>
2025-04-25 13:45:22 +0300

    Update PlaceAdapter

--- city-explorer/app/src/main/java/com/kzeen/cityexplorer/ui/adapter/PlaceAdapter.java
+++ city-explorer/app/src/main/java/com/kzeen/cityexplorer/ui/adapter/PlaceAdapter.java
@@ -3,6 +3,8 @@ package com.kzeen.cityexplorer.ui.adapter;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.kzeen.cityexplorer.R;
 import com.kzeen.cityexplorer.databinding.RowPlaceBinding;
 import com.kzeen.cityexplorer.model.Place;
 
@@ -34,6 +36,12 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> 
         Place p = data.get(pos);
         h.binding.rowTitle.setText(p.name);
         h.binding.rowSubtitle.setText(p.description);
+
+        Glide.with(h.itemView)
+                .load(p.imageURL)
+                .placeholder(R.drawable.ic_placeholder)
+                .error(R.drawable.ic_error)
+                .into(h.binding.rowThumbnail);
     }
 
     @Override public int getItemCount() { return data.size(); }
